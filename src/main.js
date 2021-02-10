0ae98130d19a1d0e043c9858777a6a2e9342676f
plyolik <p.lyolik@mail.ru>
2021-02-10 19:34:40 +0400

    add: modal-window

--- src/main.js
+++ src/main.js
@@ -12,3 +12,42 @@ import './images/icons/2x2.png'
 import './images/icons/amedia.png'
 import './images/icons/fill1.png'
 import './images/icons/fill2.png'
+
+const contentMovies = document.getElementById('content-movies');
+const contentTelevision = document.getElementById('content-television');
+const activeContent = document.getElementById('video');
+const passiveContent = document.getElementById('tele');
+
+function moviesDisplay() {
+    
+    contentMovies.classList.remove('display-none')
+    contentMovies.classList.add('display-block')
+    activeContent.classList.remove('title')
+    activeContent.classList.add('activ-content')
+
+    contentTelevision.classList.add('display-none')
+    contentTelevision.classList.remove('display-block')
+    passiveContent.classList.add('title')
+    passiveContent.classList.remove('activ-content')
+}
+
+function televosionDisplay() {
+    contentMovies.classList.remove('display-block')
+    contentMovies.classList.add('display-none')
+    activeContent.classList.remove('activ-content')
+    activeContent.classList.add('title')
+
+    contentTelevision.classList.remove('display-none')
+    contentTelevision.classList.add('display-block')
+    passiveContent.classList.remove('title')
+    passiveContent.classList.add('activ-content')
+}
+
+activeContent.addEventListener('click',  () => {
+     moviesDisplay()
+})
+
+passiveContent.addEventListener('click', ()=> {
+    televosionDisplay() 
+})
+

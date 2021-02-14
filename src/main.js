22e687140ecd68a6da6e525fb047a5e43ca50471
plyolik <p.lyolik@mail.ru>
2021-02-15 00:22:42 +0400

    add login logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './scss/style.scss'
 import 'bootstrap'
+import { Modal } from 'bootstrap/dist/js/bootstrap'
 import './images/logo/sign.png'
 import './images/logo/vector.png'
 import './images/posters/hollywood.png'
@@ -9,12 +10,27 @@ import './images/posters/striptease.png'
 import './images/icons/one.png'
 import './images/icons/2x2.png'
 import './images/icons/amedia.png'
-import './images/icons/fill1.png'
-import './images/icons/fill2.png'
+import './images/icons/rbc.png'
 
 
 const tabs = Array.from(document.querySelectorAll('.tab'));
 const contents = Array.from(document.querySelector('.content').children)
+
+const openModalBtn = document.querySelector('#open-modal')
+const modal = new Modal(document.querySelector('#exampleModal'))
+
+const loginBtn = document.querySelector('.btn-modal > button')
+const loginInput = document.querySelector('#login-field')
+
+function login() {
+    const str = loginInput.value
+    if (!str) {
+        alert('error')
+        return
+    }
+    localStorage.setItem('username', str)
+    modal.toggle()
+}
 
 function disableTabs() {
     tabs.forEach((tab) => {
@@ -33,9 +49,18 @@ function init() {
         tab.addEventListener('click', ()=> {
             disableTabs()
             hideContents()
-            tab.classList.add('.tab--active')
+            tab.classList.add('tab--active')
             contents[tabs.indexOf(tab)].classList.remove('display-none')
         })
+    })
+
+    openModalBtn.addEventListener('click', () => {
+        modal.show()
+    })
+
+    loginBtn.addEventListener('click', e => {
+        e.preventDefault();
+        login()
     })
 }
 
